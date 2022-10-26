55aa489d69ebef8f74da9d250b9e947fdcf86191
Divya-jeet <divyajeet705033@gamil.com>
2022-10-27 02:28:58 +0530

    My Mongo Assignment3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Divyajeetkumar:12345@divyajeet-db.mwc9rio.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
