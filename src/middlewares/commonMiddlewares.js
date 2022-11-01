ea22d360f2a5c28adec26dac6ad250b8bc7c5685
Divya-jeet <divyajeet705033@gamil.com>
2022-11-01 23:44:14 +0530

    MiddleWare Assignment is Done

--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -17,8 +17,10 @@ const mid3= function ( req, res, next) {
 
 const mid4= function ( req, res, next) {
     console.log("Hi I am a middleware named Mid4")
+
     next()
 }
+
 
 module.exports.mid1= mid1
 module.exports.mid2= mid2
