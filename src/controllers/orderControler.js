d71223fdb8f5a581fec73f0e65e919721c9f1b2a
Divya-jeet <divyajeet705033@gamil.com>
2022-11-03 00:03:19 +0530

    MiddleWare Assignment

---
+++ src/controllers/orderControler.js
@@ -0,0 +1,47 @@
+const orderModel= require("../models/orderModel")
+const productModel = require("../models/productModel")
+const userModel = require("../models/userModel")
+
+const createOrder= async function (req, res) {
+
+    let data=req.body
+    let value = req.headers.isfreeappuser
+    data.isFreeAppUser=value
+    let userId= data.userId
+    let productId=data.productId 
+    if(!userId){
+        return res.send({msg: "userid is mandatory"})
+    }else if(!productId){
+        return res.send({msg: "productId is mandatory"}) 
+    } 
+
+    let userid = await userModel.findById(data.userId)
+    if(!userid){
+        return res.send({msg: "userId is not valid"})
+    }
+    let productid= await productModel.findById(data.productId)
+    if(!productid){
+        return res.send({msg: "productId is not valid"})
+    } 
+
+    let num=0
+    if(value== "true"){
+        data.amount=num
+        let savedData=await orderModel.create(data)
+        res.send({data: savedData})
+    }
+    else if(userid.balance>= productid.price){
+        await userModel.findOneAndUpdate({_id: userid}, 
+            {$set:{balance:userid.balance - productid.price}})
+            data['amount']= productid.price;
+            data['isFreeAppUser']= req.headers.isfreeappuser;
+
+    
+     let savedData= await orderModel.create(data) 
+     res.send({data: savedData}) 
+    } else {
+        return res.send({msg: "user have not enough balance"})
+     }
+    }
+
+module.exports.createOrder= createOrder
--- src/index.js
+++ src/index.js
@@ -8,23 +8,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://princekumar2022:ktbaRBP25CkJa0L0@cluster0.kcuyi1y.mongodb.net/pk-22?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// Move around the following block and see how its order impacts what gets called first
+
+
+app.use('/', route);
+
 app.use (
     function (req, res, next) {
         console.log ("inside GLOBAL MW");
-        next()
-        // What happens if we send a response instead of the next() call ?
-        //res.send({msg:"done"})
+        res.send({msg:"done"})
   }
-);
+  );
 
-app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
