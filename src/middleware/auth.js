5732c244771b1d6dcd2cf57fa5d8b041eeaebf14
Divya-jeet <divyajeet705033@gamil.com>
2022-11-06 23:20:31 +0530

    my second assignment of auth2 is done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,49 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
+
+
+const authenticate = function (req, res, next) {
+    try {
+        let token = req.headers["x-Auth-token"];
+     if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
+  
+    console.log(token);
+
+    let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
+    if (!decodedToken) {
+      return res.status(403).send({ status: false, msg: "token is invalid" });
+    }
+    req.userLoggedIn=decodedToken.userId
 
     next()
+        
+    } catch (error) {
+        return res.status(500).send({msg: "SERVER ISSUE"})
+        
+    }
+    
+    
+   
 }
 
 
 const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
+    try {
+        let requestUserId = req.params.userId
+        if(requestUserId != req.userLoggedIn){ return res.status(403).send({status: false, msg: "permission denied"})
+    }
+        next()
+        
+    } catch (error) {
+        return res.status(500).send({msg: "SERVER ISSUE"})
+        
+    }
+   
 }
+
+
+
+
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
