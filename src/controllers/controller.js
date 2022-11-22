960819b52f830a04652440d52d2ffa01b68208d1
Divya-jeet <divyajeet705033@gamil.com>
2022-11-22 17:16:48 +0530

    Happy Flow Ok

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -9,9 +9,9 @@ const createCollege = async function (req, res) {
     try {
         let data = req.body
         let { name, fullName, logoLink } = data
-        
+
         if (!name) { return res.status(400).send({ status: false, message: "name field is mandatory" }) }
-        if (!fullname) { return res.status(400).send({ status: false, message: "fullname field is mandatory" }) }
+        if (!fullName) { return res.status(400).send({ status: false, message: "fullname field is mandatory" }) }
         if (!logoLink) { return res.status(400).send({ status: false, message: "logoLink field is mandatory" }) }
 
         //---------checking the mandatory fields------------------
@@ -34,22 +34,23 @@ const createCollege = async function (req, res) {
         if (!(regForName(name))) { return res.status(400).send({ status: false, message: "invalid name" }) }
         //---------validation of fullname---------
 
+
         if ((valid(fullName) == false)) { return res.status(400).send({ status: false, message: "provide a valid fullname" }) }
         // console.log(regForFullName(fullName))
+
         if ((regForFullName(fullName) == false)) { return res.status(400).send({ status: false, message: "Fullname can contain only Alphabets, commas and spaces!" }) }
         //------validation of logo link--------------------------
+
         if ((valid(logoLink) == false)) { return res.status(400).send({ status: false, message: "provide a valid logo link" }) }
         if (regForLink(logoLink) == false) { return res.status(400).send({ status: false, message: "invalid link" }) }
         //  console.log(regForLink(logoLink))
         req.body.name = name
         req.body.fullName = fullName
-        // req.body.logoLink=logoLink
-        //console.log(req.body)
 
         //-------creating college data-------------
         let collegeData = await collegeModel.create(req.body)
 
-        //  console.log(collegeData)
+
         res.status(201).send({ status: true, data: collegeData })
     }
     catch (error) {
@@ -60,7 +61,7 @@ const createCollege = async function (req, res) {
 
 ///2-----------------------createIntern-----------------------
 const createIntern = async function (req, res) {
-    try{
+    try {
         const userInput = req.body
         const { name, mobile, email, collegeName } = userInput
 
@@ -72,15 +73,22 @@ const createIntern = async function (req, res) {
         // if (!(valid(name))) { return res.status(400).send({ status: false, message: "provide a valid name" }) }
         if ((regForName(name) == false)) { return res.status(400).send({ status: false, message: "invalid name" }) }
         //-----------------------validation of email---------------------------------------
-        if (regForEmail(email) == false) { return res.status(400).semd({ status: false, mrssage: "provide a valid email" }) }
+       if (regForEmail(email) == false) { return res.status(400).send({ status: false, message: "provide a valid email" }) }
+
         //-----------------validation of mobile number--------------------------
         if ((regForMobileNo(mobile) == false)) { return res.status(400).send({ status: false, message: "provide a valid mobile number" }) }
+
+        let checkDuplicateemail = await internModel.findOne({ email: email })
+        if (checkDuplicateemail) { return res.status(400).send({ status: false, message: "the email is exist please provide another email" }) }
+
+        let checkDuplicatemobile = await internModel.findOne({ mobile: mobile })
+        if (checkDuplicatemobile) { return res.status(400).send({ status: false, message: "the mobile No is exist please provide another Mobile No" }) }
         //-----------------validation of college name---------
         console.log(regForMobileNo(mobile))
         if (req.body.collegeName) {
 
-           
-            let collegeNamef= collegeName.trim().toLowerCase()
+
+            let collegeNamef = collegeName.trim().toLowerCase()
             console.log(collegeName)
             if (!valid(req.body.collegeName)) { res.status(400).send({ status: false, message: "enter valid collegeName" }) }
             let present = await collegeModel.findOne({ name: collegeNamef })
@@ -109,12 +117,12 @@ const createIntern = async function (req, res) {
         } else {
             return res.status(400).send({ status: false, message: "collegeId or collegeName must be present" })
         }
-    }catch(error) {
-        return res.status(500).send({status:false,message:error.message})
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
     }
-    }
-  
-    
+}
+
+
 
 //------------------getcollegeDetails----------------------------
 const getcollegeData = async function (req, res) {
@@ -124,19 +132,33 @@ const getcollegeData = async function (req, res) {
         if (!input1) { res.status(400).send({ status: false, message: "Please enter collegeName in queryParam" }) }
 
         //Dbcall for getting the CollegeId from Name
-        let collegeDetail = await collegeModel.findOne({name:input1})
-        if(!collegeDetail) return res.status(400).send({status:false,msg:"college not found"})
-        let id=collegeDetail._id.toString()
+        let collegeDetail = await collegeModel.findOne({ name: input1 })
+        if (!collegeDetail) return res.status(400).send({ status: false, msg: "college not found" })
+        let id = collegeDetail._id.toString()
 
         //-------------Dbcall for grtting internlist from collegeId--------------
-        const internList = await internModel.find({collegeId:id})
-        console.log(internList)
-        let obj={}
-        obj.interns=internList
+        const internList = await internModel.find({ collegeId: id })
+        if (internList.length !== 0) { 
+        // console.log(internList)
+        let obj = {}
+        obj.name = collegeDetail.name
+        obj.fullname = collegeDetail.fullName
+        obj.logoLink = collegeDetail.logoLink
+        obj.interns = internList
         // collegeDetail.interns = internList
-        obj.name=collegeDetail.fullName
-        obj.id=collegeDetail._id
+
         res.status(200).send({ status: true, data: obj })
+    
+    }else{
+        let obj = {}
+        obj.name = collegeDetail.name
+        obj.fullname = collegeDetail.fullName
+        obj.logoLink = collegeDetail.logoLink
+        obj.interns = "No Interns Found"
+        // collegeDetail.interns = internList
+
+        res.status(200).send({ status: true, data: obj })
+    }
 
         // if(!result){res.status(404).send({status:false , message:"Details not found."})}
 
